2d76227534477ce714480a0b30c02195bff41e49
Nathan <nathan.agu.2103@gmail.com>
2025-04-04 21:17:57 +0200

    fix: Utilisation de middlewares non existant dans server.js

--- api/app.js
+++ api/app.js
@@ -10,11 +10,7 @@ const routes = require("./routes");
 const app = express();
 
 // Middlewares globaux
-app.use(cors());
-app.use(helmet());
-app.use(morgan("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.use("/api", routes);
